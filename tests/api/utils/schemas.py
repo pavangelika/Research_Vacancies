76010af6f6068fae8f66765dd560dbdcbe44a58a
pavangelika <pavangelika@gmail.com>
2026-02-09 16:37:14 +0500

    add contract test

--- tests/api/utils/schemas.py
+++ tests/api/utils/schemas.py
@@ -15,89 +15,89 @@ VACANCIES_SCHEMA = {
                              "published_at", "relations", "response_letter_required", "salary_range",
                              "type", "url", "snippet"],
                 "properties": {
-                    "accept_incomplete_resumes":{"type":"boolean"},
-                    "alternate_url":{"type":"string"},
-                    "apply_alternate_url":{"type":"string"},
-                    "area":{
-                        "type":["object"],
-                        "required":["id", "name", "url"],
-                        "properties":{
-                            "id":{"type":"string"},
-                            "name":{"type":"string"},
-                            "url":{"type":"string"},
+                    "accept_incomplete_resumes": {"type": "boolean"},
+                    "alternate_url": {"type": "string"},
+                    "apply_alternate_url": {"type": "string"},
+                    "area": {
+                        "type": ["object"],
+                        "required": ["id", "name", "url"],
+                        "properties": {
+                            "id": {"type": "string"},
+                            "name": {"type": "string"},
+                            "url": {"type": "string"},
                         }
                     },
-                    "department":{
-                        "type":["object", "null"],
-                        "properties":{
-                            "id":{"type":"string"},
-                            "name":{"type":"string"}
-                        }
-                    },
-                    "employer":{
-                        "type":["object"],
-                        "required":["name", "trusted"],
-                        "properties":{
-                            "accredited_it_employer":{"type":"boolean"},
-                            "alternate_url":{"type":["string", "null"]},
-                            "country_id":{"type":"integer"},
-                            "employer_rating": {
-                                "type": ["object", "null"],
-                                "required":["reviews_count", "total_rating"],
-                                "properties": {
-                                    "reviews_count":{"type":["string", "integer", "number", "null"]},
-                                    "total_rating":{"type":"string"},
-                                }
-                            },
-                            "id":{"type":["string", "null"]},
-                            "is_identified_by_esia":{"type":"boolean"},
-                            "logo_urls":{"type":["null", "object"]},
-                            "name":{"type":"string"},
-                            "trusted":{"type":"boolean"},
-                            "url":{"type":["string", "null"]},
-                            "vacancies_url":{"type":["string", "null"]},
-                        }
-                    },
-                    "has_test":{"type":"boolean"},
-                    "id":{"type":"string"},
-                    "name":{"type":"string"},
-                    "professional_roles":{
-                        "type":["array", "object"],
+                    "department": {
+                        "type": ["object", "null"],
                         "properties": {
                             "id": {"type": "string"},
                             "name": {"type": "string"}
                         }
                     },
-                    "published_at":{"type":"string"},
-                    "relations":{"type":["null", "array", "string"]},
-                    "response_letter_required":{"type":"boolean"},
+                    "employer": {
+                        "type": ["object"],
+                        "required": ["name", "trusted"],
+                        "properties": {
+                            "accredited_it_employer": {"type": "boolean"},
+                            "alternate_url": {"type": ["string", "null"]},
+                            "country_id": {"type": "integer"},
+                            "employer_rating": {
+                                "type": ["object", "null"],
+                                "required": ["reviews_count", "total_rating"],
+                                "properties": {
+                                    "reviews_count": {"type": ["string", "integer", "number", "null"]},
+                                    "total_rating": {"type": "string"},
+                                }
+                            },
+                            "id": {"type": ["string", "null"]},
+                            "is_identified_by_esia": {"type": "boolean"},
+                            "logo_urls": {"type": ["null", "object"]},
+                            "name": {"type": "string"},
+                            "trusted": {"type": "boolean"},
+                            "url": {"type": ["string", "null"]},
+                            "vacancies_url": {"type": ["string", "null"]},
+                        }
+                    },
+                    "has_test": {"type": "boolean"},
+                    "id": {"type": "string"},
+                    "name": {"type": "string"},
+                    "professional_roles": {
+                        "type": ["array", "object"],
+                        "properties": {
+                            "id": {"type": "string"},
+                            "name": {"type": "string"}
+                        }
+                    },
+                    "published_at": {"type": "string"},
+                    "relations": {"type": ["null", "array", "string"]},
+                    "response_letter_required": {"type": "boolean"},
                     "salary_range": {
                         "type": ["object", "null"],
-                        "required":["currency", "gross", "mode"],
+                        "required": ["currency", "gross", "mode"],
                         "properties": {
-                            "currency": {"type":"string"},
-                            "gross": {"type":"boolean"},
-                            "mode": {"type":"object"},
+                            "currency": {"type": "string"},
+                            "gross": {"type": "boolean"},
+                            "mode": {"type": "object"},
                             "from": {"type": ["number", "null"]},
                             "to": {"type": ["number", "null"]}
                         }
                     },
-                    "type":{
+                    "type": {
                         "type": ["object"],
-                        "required":["id"],
+                        "required": ["id"],
                         "properties": {
-                            "id": {"type":"string"},
-                            "name": {"type":"string"}
+                            "id": {"type": "string"},
+                            "name": {"type": "string"}
                         }
                     },
-                    "url":{"type":"string"},
-                    "snippet":{
+                    "url": {"type": "string"},
+                    "snippet": {
                         "type": ["object"],
                         "properties": {
-                            "requirement": {"type":["string", "null"]},
-                            "responsibility": {"type":["string", "null"]}
+                            "requirement": {"type": ["string", "null"]},
+                            "responsibility": {"type": ["string", "null"]}
                         }
-                   }
+                    }
 
                 }
             }
--- tests/api/vacancies/test_vacancies_contract.py
+++ tests/api/vacancies/test_vacancies_contract.py
@@ -23,79 +23,78 @@ from tests.api.utils.schemas import validate_vacancies_response, VACANCY_DETAIL_
 @pytest.mark.vacancies
 @allure.title("Валидация JSON схемы ответа")
 def test_vacancy_validation(api_client, attach_headers_request_response):
-        """Контрактный тест - проверка соответствия JSON Schema."""
-        with allure.step(f"#1 Получить список вакансий с выбранными параметрами фильтрации"):
-            params = {
-                "host": "hh.ru",
-                "per_page": 100,
-                "page": 0,
-                "period": 1,
-                "order_by": "salary_desc",
-                "professional_role": 124,
-                "work_format": "REMOTE"
-            }
-            response = api_client.get_vacancies(**params)
-            list_vacancies = response.json()
+    """Контрактный тест - проверка соответствия JSON Schema."""
+    with allure.step(f"#1 Получить список вакансий с выбранными параметрами фильтрации"):
+        params = {
+            "host": "hh.ru",
+            "per_page": 100,
+            "page": 0,
+            "period": 1,
+            "order_by": "salary_desc",
+            "professional_role": 124,
+            "work_format": "REMOTE"
+        }
+        response = api_client.get_vacancies(**params)
+        list_vacancies = response.json()
 
-            attach_headers_request_response(
-                "GET",
-                "https://api.hh.ru/vacancies",
-                params,
-                dict(api_client.session.headers),
-                response)
+        attach_headers_request_response(
+            "GET",
+            "https://api.hh.ru/vacancies",
+            params,
+            dict(api_client.session.headers),
+            response)
 
-        with allure.step("#2 Проверить: ответ в формате JSON "):
-            assert 'application/json' in response.headers.get('Content-Type', '').lower()
-            allure.attach(
-                body=f"Response type,{response.headers.get('Content-Type', '').lower()}",
-                name="Response type",
-                attachment_type=allure.attachment_type.CSV
-            )
+    with allure.step("#2 Проверить: ответ в формате JSON "):
+        assert 'application/json' in response.headers.get('Content-Type', '').lower()
+        allure.attach(
+            body=f"Response type,{response.headers.get('Content-Type', '').lower()}",
+            name="Response type",
+            attachment_type=allure.attachment_type.CSV
+        )
+        allure.attach(
+            body=json.dumps(list_vacancies, indent=2, ensure_ascii=False),
+            name="Response Body",
+            attachment_type=allure.attachment_type.JSON
+        )
+
+    with allure.step("#3 Структура ответа соответсвует JSON схеме"):
+        try:
+            validate_vacancies_response(list_vacancies)
+        except Exception as e:
             allure.attach(
                 body=json.dumps(list_vacancies, indent=2, ensure_ascii=False),
-                name="Response Body",
+                name="Invalid Response",
                 attachment_type=allure.attachment_type.JSON
             )
+            pytest.fail(f"Ошибка валидации схемы: {str(e)}")
 
-        with allure.step("#3 Структура ответа соответсвует JSON схеме"):
-            try:
-                validate_vacancies_response(list_vacancies)
-            except Exception as e:
-                allure.attach(
-                    body=json.dumps(list_vacancies, indent=2, ensure_ascii=False),
-                    name="Invalid Response",
-                    attachment_type=allure.attachment_type.JSON
-                )
-                pytest.fail(f"Ошибка валидации схемы: {str(e)}")
+    with allure.step("#4 Типы данных обязательных полей и наличие соответсвуют JSON схеме"):
 
-        with allure.step("#4 Типы данных обязательных полей и наличие соответсвуют JSON схеме"):
+        validation_results = check_data_types(list_vacancies, VACANCIES_SCHEMA)
 
-            validation_results = check_data_types(list_vacancies, VACANCIES_SCHEMA)
-
-            csv_rows = ["Поле, Наличие, ФР, ОР"]
-            for result in validation_results:
-                csv_rows.append(
-                    f"{result['field']}, "
-                    f"{result['status']}, "
-                    f"{result['actual_type']}, "
-                    f"{result['expected_type']}, "
-                )
-
-            allure.attach(
-                body="\n".join(csv_rows),
-                name="Field Validation Summary",
-                attachment_type=allure.attachment_type.CSV
+        csv_rows = ["Поле, Наличие, ФР, ОР"]
+        for result in validation_results:
+            csv_rows.append(
+                f"{result['field']}, "
+                f"{result['status']}, "
+                f"{result['actual_type']}, "
+                f"{result['expected_type']}, "
             )
 
-            required_fields = VACANCIES_SCHEMA.get("required", [])
-            missing_fields = [field for field in required_fields
-                              if field not in list_vacancies]
+        allure.attach(
+            body="\n".join(csv_rows),
+            name="Field Validation Summary",
+            attachment_type=allure.attachment_type.CSV
+        )
 
-            if missing_fields:
-                allure.attach(
-                    body=f"Отсутствующие обязательные поля: {missing_fields}",
-                    name="Missing Required Fields",
-                    attachment_type=allure.attachment_type.TEXT
-                )
-                assert not missing_fields, f"Отсутствуют обязательные поля: {missing_fields}"
+        required_fields = VACANCIES_SCHEMA.get("required", [])
+        missing_fields = [field for field in required_fields
+                          if field not in list_vacancies]
 
+        if missing_fields:
+            allure.attach(
+                body=f"Отсутствующие обязательные поля: {missing_fields}",
+                name="Missing Required Fields",
+                attachment_type=allure.attachment_type.TEXT
+            )
+            assert not missing_fields, f"Отсутствуют обязательные поля: {missing_fields}"
